7c772e6d98d7713bb12c5f4fc7e6c15404c57dcf
Daniel Santos <dados.dan.santos@gmail.com>
2021-10-26 20:51:53 -0300

    feat: add carousel images

---
+++ static/js/carousel-cotroller.js
@@ -0,0 +1,12 @@
+$("#banner-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  items: 1,
+  autoplay: true,
+  autoplayTimeout: 7000,
+  itemElement: "section",
+  navElement: "span",
+  dots: true,
+  animateOut: "fadeOut",
+});
