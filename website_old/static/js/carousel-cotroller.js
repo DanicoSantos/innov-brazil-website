9ad95351d6c2b5c1f097a7b301fd898dfe57031e
Daniel Santos <dados.dan.santos@gmail.com>
2021-10-30 16:07:38 -0300

    feat: archive old files

---
+++ website_old/static/js/carousel-cotroller.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function ($) {
+  $("#banner-carousel").owlCarousel({
+    // loop: true,
+    // smartSpeed: 450,
+    margin: 30,
+    // nav: false,
+    items: 1,
+    // itemElement: "section",
+    // navElement: "span",
+    // dots: true,
+    animateOut: "puff-out-center",
+    animateIn: "puff-in-center",
+  });
+});
