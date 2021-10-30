c7371b8df6a7f076b532da9ae6e0b723df9b2fe8
Daniel Santos <dados.dan.santos@gmail.com>
2021-10-30 18:07:13 -0300

    feat: add logo carousel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+  $("#partners").owlCarousel({
+    loop: true,
+    // smartSpeed: 450,
+    margin: 30,
+    nav: false,
+    items: 5,
+    itemElement: "div",
+    // navElement: "span",
+    // dots: true,
+  });
+});
