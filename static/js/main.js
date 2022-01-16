8996fb252dad5656436faf1370686d2a892c524f
Daniel Santos <dados.dan.santos@gmail.com>
2022-01-16 15:24:42 -0300

    feat: add lng to partner links

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,21 @@
 jQuery(document).ready(function ($) {
   const mobileMenuToggler = document.querySelector("#menuToggler");
   const lngQueryString = location.search;
+  const path = location.pathname;
 
   if (!lngQueryString) {
     location.search = "en";
-  }   
-
+  }
+  
+  if (path.includes('partners')) {
+    const partnerLinks = $('#partners [href]');
+    for (let item of partnerLinks) {
+      let $item = $(item);
+      let newItemLink = $item.attr('href') + location.search;
+      $item.attr('href', newItemLink);
+    }
+  }
+  
   /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
   function toggleMobileMenu() {
     var x = document.getElementById("NavLinks");
